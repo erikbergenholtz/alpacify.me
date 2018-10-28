f0ce5c3e8c8f6af77c61cad687065dcf95c7274f
Erik Bergenholtz <ebz@bth.se>
2018-10-28 21:08:51 +0100

    Add alpacification

--- alpacify/alpacify.py
+++ alpacify/alpacify.py
@@ -4,13 +4,26 @@ from PIL import Image
 import cv2 as cv
 import numpy as np
 import base64
+import random
 
 def detectFaces(img):
     gray = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-    dirname = os.path.dirname(__file__)
-    cascade = os.path.join(dirname, 'cascade.xml')
+    cascade = os.path.join(os.path.dirname(__file__), 'cascade.xml')
     face_cascade = cv.CascadeClassifier(cascade)
     return face_cascade.detectMultiScale(gray, 1.3, 5)
+
+def pasteAlpaca(o_img, x, y, w, h):
+    alpacadir = os.path.join(os.path.dirname(__file__), 'alpacas')
+    img_path = os.path.join(alpacadir, random.choice(os.listdir(alpacadir)))
+    a_img = cv.resize(np.array(Image.open(img_path)), (w, h))
+    y1, y2 = y, y+a_img.shape[0]
+    x1, x2 = x, x+a_img.shape[1]
+    alpha_a = a_img[:, :, 3] / 255.0
+    alpha_o = 1.0 - alpha_a
+    for c in range(0,3):
+        o_img[y1:y2, x1:x2, c] = (alpha_a * a_img[:, :, c] +
+                                  alpha_o * o_img[y1:y2, x1:x2, c])
+
 
 def alpacify(data):
     sbuf = BytesIO()
@@ -20,7 +33,7 @@ def alpacify(data):
     mode = img.mode
     img = np.array(img)
     for (x,y,w,h) in detectFaces(img):
-        cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0, 255), 2)
+        pasteAlpaca(img,x,y,w,h)
     out = Image.fromarray(img, mode)
     outp = BytesIO()
     out.save(outp, format=format)
